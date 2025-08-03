f5cb17cfc87f8b25be60b8a015eec0668bf7a326
Aryan Shekhada <shekhadaaryan37@gmail.com>
2025-08-03 10:57:29 +0530

    defualt inamge add

--- chat/send_message.php
+++ chat/send_message.php
@@ -5,8 +5,8 @@ $sender = $_POST['sender_id'];
 $receiver = $_POST['receiver_id'];
 $message = trim($_POST['message']);
 
-if (!empty($message)) {
-    $stmt = $conn->prepare("INSERT INTO messages (sender_id, receiver_id, message, is_read, sent_at) VALUES (?, ?, ?, 0, NOW())");
-    $stmt->bind_param("iis", $sender, $receiver, $message);
-    $stmt->execute();
-}
+// if (!empty($message)) {
+//     $stmt = $conn->prepare("INSERT INTO messages (sender_id, receiver_id, message, is_read, sent_at) VALUES (?, ?, ?, 0, NOW())");
+//     $stmt->bind_param("iis", $sender, $receiver, $message);
+//     $stmt->execute();
+// }
